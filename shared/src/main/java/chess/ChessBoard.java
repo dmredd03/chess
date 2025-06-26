1c5b88ebc92a099a825f75739d5c4d4d8e3d1fcb
dmredd03 <dmredd03@gmail.com>
2025-06-26 00:50:21 -0600

    Completed ChessBoardTests and started on Bishop piece implementation

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -12,6 +15,20 @@ public class ChessBoard {
         
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.deepEquals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(squares);
+    }
+
     /**
      * Adds a chess piece to the chessboard
      *
@@ -19,7 +36,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        squares[position.getRow()][position.getColumn()] = piece;
+        squares[position.getRow()-1][position.getColumn()-1] = piece;
     }
 
     /**
@@ -30,7 +47,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        return squares[position.getRow()][position.getColumn()];
+        return squares[position.getRow()-1][position.getColumn()-1];
     }
 
     /**
@@ -38,6 +55,34 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        squares = new ChessPiece[8][8];
+        // white pawn line
+        for (int i = 1; i <= 8; i++) {
+            addPiece(new ChessPosition(2, i), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+        }
+        // white pieces
+        addPiece(new ChessPosition(1, 1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(1, 2), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(1, 3), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(1, 4), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(1, 5), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(1, 6), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(1, 7), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(1, 8), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+
+        // black pawn line
+        for (int i = 1; i <= 8; i++) {
+            addPiece(new ChessPosition(7, i), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+        }
+        // black piece line
+        addPiece(new ChessPosition(8, 1), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(8, 2), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(8, 3), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(8, 4), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(8, 5), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(8, 6), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(8, 7), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(8, 8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
     }
+
 }
--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,4 +1,60 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class PieceMovesCalculator {
+    private ChessPosition startPosition;
+    private Collection<ChessMove> possibleMoves;
+    private ChessBoard board;
+    private ChessPiece piece;
+
+    public PieceMovesCalculator(ChessBoard board, ChessPosition position) {
+        this.startPosition = position;
+        this.board = board;
+        this.possibleMoves = new ArrayList<>();
+    }
+
+    public Collection<ChessMove> getPossibleMoves() {
+        this.piece = board.getPiece(startPosition);
+        if (piece.getPieceType() == ChessPiece.PieceType.BISHOP) {
+            BishopMovesCalculator();
+        }
+
+        return possibleMoves;
+    }
+
+    private void BishopMovesCalculator() {
+        int currRow = startPosition.getRow();
+        int currCol = startPosition.getColumn();
+
+        // up-right direction checks
+        currRow++;
+        currCol++;
+        while (currRow < 8 && currCol < 8) {
+
+            ChessPosition currChessMovePosition = new ChessPosition(currRow, currCol);
+            ChessPiece chessPiece = board.getPiece(currChessMovePosition);
+            // if empty
+            if (chessPiece == null) {
+                ChessMove possibleMove = new ChessMove(startPosition, currChessMovePosition, ChessPiece.PieceType.BISHOP);
+                possibleMoves.add(possibleMove);
+                // if enemy
+            } else if (chessPiece.getTeamColor() != piece.getTeamColor()) {
+
+                ChessMove possibleMove = new ChessMove(startPosition, currChessMovePosition, ChessPiece.PieceType.BISHOP);
+                possibleMoves.add(possibleMove);
+                break;
+                // if friendly piece
+            } else if (chessPiece.getTeamColor() == piece.getTeamColor()) {
+
+                break;
+            }
+            currRow++;
+            currCol++;
+        }
+
+    }
+
+
 }
